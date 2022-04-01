cc7bacc85b5ec1699de1eb61a6d60726930ef9c9
jannpey <hfw6915@autuni.ac.nz>
2022-04-01 14:42:26 +0800

    Updated Components Code and package.json

--- app.js
+++ app.js
@@ -23,8 +23,20 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 5000;
+// Accessing the path module
+const path = require("path");
 
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// Step 2:
+app.get("*", function (request, response) {
+
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+
+});
+
+const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
--- client/src/components/ShowBookDetails.js
+++ client/src/components/ShowBookDetails.js
@@ -28,7 +28,7 @@ class showBookDetails extends Component {
 
   onDeleteClick (id) {
     axios
-      .delete('http://localhost:8082/api/books/'+id)
+      .delete('https://jann-worksheet3.herokuapp.com/api/books/'+id)
       .then(res => {
         this.props.history.push("/");
       })
